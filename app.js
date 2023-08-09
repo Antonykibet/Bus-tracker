a03b0e659739808ab9309ccefb31279e1b2e772e
Antony Kibet <antony.kibet@metropol.local>
2023-08-09 17:16:36 +0300

    added admin page, set get and update functionalities, intergrated Mongodb1

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const {MongoClient} = require('mongodb')
 const path =require('path')
 
 const app = express()
 const PORT=3500
+const uri = "mongodb+srv://antonykibet059:123Acosta@cluster0.eoos6vz.mongodb.net/?retryWrites=true&w=majority";
+const dbClient = new MongoClient(uri) 
+let db;
+let collection;
 
 app.use(express.json())
 app.use(express.static('./public'))
@@ -14,9 +19,21 @@ app.get('/',(req,res)=>{
 app.get('/admin',(req,res)=>{
     res.sendFile(path.join(__dirname,'admin.html') )
 })
-// app.post('/update',(req,res)=>{
-//     console.log(req.body)
-//     res.send('test')
-// })
 
-app.listen(PORT,()=>console.log(`Server is listening at port ${PORT}`))
+app.get('/getLocation',async(req,res)=>{
+    let coords = await collection.findOne({name:'Bus1'})
+    console.log(coords)
+    res.json(coords)
+})
+app.post('/update',async(req,res)=>{
+    let coords = req.body
+    await collection.updateOne({name:'Bus1'},{$set:{location:coords}})
+})
+
+app.listen(PORT, async()=>{
+    await dbClient.connect()
+    db = await dbClient.db('Bus-Tracker')
+    collection= await db.collection('location')
+    console.log('Db Connected...')
+    console.log(`Server is listening at port ${PORT}...`
+    )})
--- public/script.js
+++ public/script.js
@@ -1,23 +1,34 @@
+let result = null
+let coords ={
+  lat:-1.36525 ,
+  lng: 36.92412,
+}
+
 var platform = new H.service.Platform({
-    'apikey': ''
+    'apikey': 'K6fed5L_aps2P7_K82K54HsJpQkReOe7BRHecXOGMXA'
   });
 
 var defaultLayers = platform.createDefaultLayers();
   
 var map = new H.Map(document.getElementById('map'),
-              defaultLayers.vector.normal.map,
-              {
-                zoom: 19,
-                // center: { lat: 65.7, lng: 13.4 }
-              }
-              );
+    defaultLayers.vector.normal.map,
+    {
+      zoom: 19,
+    },
+    );
 
 const icon = new H.map.Icon('./icons/icons8-bus-30.png')
-let cords ={
-  lat:-1.36525 ,
-  lng: 36.92412,
-}
-const marker =new H.map.Marker(cords,{icon:icon})
-
+let marker =new H.map.Marker(coords,{icon:icon})
 map.addObject(marker);
-map.setCenter(cords);
+map.setCenter(coords);
+
+async function getLocation(){
+  let response = await fetch('/getLocation')
+  coords =await response.json() 
+  let {location} = coords
+  marker =new H.map.Marker(location,{icon:icon})
+  
+  map.setCenter(location);
+}
+getLocation()
+//setInterval(getLocation,60000)
